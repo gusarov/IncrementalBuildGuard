0589928e4941607cf65cc0857cc8c1e1bc834960
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2020-05-29 16:46:13 -0400

    improvements

--- IncrementalBuildGuard/ibg.cmd
+++ IncrementalBuildGuard/ibg.cmd
@@ -1,12 +1,12 @@
 @echo off
 set extra=%*
-msbuild -t:restore
+msbuild -t:restore -clp:ErrorsOnly
 if errorlevel 1 exit /b %errorlevel%
 msbuild -m %extra%
 if errorlevel 1 exit /b %errorlevel%
 set f=%tmp%\ibg.flag
 del %f%
-msbuild %extra% "-logger:IncrementalBuildGuardLogger,%~dp0IncrementalBuildGuard.dll;%f%" -clp:PerformanceSummary
+msbuild %extra% "-logger:IncrementalBuildGuardLogger,%~dp0IncrementalBuildGuard.dll;%f%" -clp:PerformanceSummary;v=q
 if errorlevel 1 exit /b %errorlevel%
 set issue=
 if exist "%f%" set issue=1
